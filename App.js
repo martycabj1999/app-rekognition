928117b35f6fd6db4456f6c29d7505df36a7725a
ivanchiabrera <44534420+ivanchiabrera@users.noreply.github.com>
2021-05-22 20:15:51 -0300

    Test de camara

--- App.js
+++ App.js
@@ -1,12 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState();
+  const [type, setType] = useState(Camera.Constants.Type.front);
+
+  const cam = useRef();
+
+  const _takePicture = async () => {
+
+    if (cam.current) {
+      const option = { quality: 0.5, base64: true, skipProcessing: false };
+      let photo = await cam.current.takePictureAsync(option);
+
+      console.log(cam.current.getSupportedRatiosAsync());
+      const source = photo.uri;
+
+      if (source) {
+        cam.current.resumePreview();
+        console.log("picture source", source);
+      }
+    }
+
+  }
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera ref={cam} style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'flex-end', alignContent: 'center' }}>
+            <View>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => _takePicture()}>
+                <Text style={styles.text}> Take </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Camera>
     </View>
   );
 }
@@ -14,8 +60,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    justifyContent: 'center', alignItems: 'flex-end', alignContent: 'center',
+    margin: 20,
+  },
+  button: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 18,
+    color: 'white',
   },
 });
